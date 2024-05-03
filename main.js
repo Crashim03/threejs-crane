9447fbe0eca1f17a383a494d0638c608db72e772
Crashim03 <alexteloduarte@gmail.com>
2024-05-03 17:16:19 +0100

    classesssssss :3

--- main.js
+++ main.js
@@ -1,166 +1,313 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 class Input {
-    constructor() {
-        document.addEventListener("keydown",  this.handleKeyDown.bind(this));
-        document.addEventListener("keyup", this.handleKeyUp.bind(this));
-    }
+  constructor() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+    document.addEventListener("keyup", this.handleKeyUp.bind(this));
+  }
 
-    handleKeyDown(event) {
-        switch (event.code) {
-            case "Digit1":
-                this.dispatchSwitchCameraEvent(0);
-                break;
-            case "Digit2":
-                this.dispatchSwitchCameraEvent(1);
-                break;
-            case "Digit3":
-                this.dispatchSwitchCameraEvent(2);
-                break;
-            case "Digit4":
-                this.dispatchSwitchCameraEvent(3);
-                break;
-            case "Digit5":
-                this.dispatchSwitchCameraEvent(4);
-                break;
-            case "Digit6":
-                this.dispatchSwitchCameraEvent(5);
-                break;
-            case "KeyQ":
-                this.dispatchRotateCraneEvent(1, true);
-                break;
-            case "KeyA":
-                this.dispatchRotateCraneEvent(-1, true);
-                break;
-            case "KeyW":
-                this.dispatchMoveCartEvent(1, true);
-                break;
-            case "KeyS":
-                this.dispatchMoveCartEvent(-1, true);
-                break;
-            case "KeyE":
-                this.dispatchElevateCraneEvent(1, true);
-                break;
-            case "KeyD":
-                this.dispatchElevateCraneEvent(-1, true);
-                break;
-            default:
-                break;
-        }
+  handleKeyDown(event) {
+    switch (event.code) {
+      case "Digit1":
+        this.dispatchSwitchCameraEvent(0, true);
+        break;
+      case "Digit2":
+        this.dispatchSwitchCameraEvent(1, true);
+        break;
+      case "Digit3":
+        this.dispatchSwitchCameraEvent(2, true);
+        break;
+      case "Digit4":
+        this.dispatchSwitchCameraEvent(3, true);
+        break;
+      case "Digit5":
+        this.dispatchSwitchCameraEvent(4, true);
+        break;
+      case "Digit6":
+        this.dispatchSwitchCameraEvent(5, true);
+        break;
+      case "KeyQ":
+        this.dispatchRotateCraneEvent(1, true);
+        break;
+      case "KeyA":
+        this.dispatchRotateCraneEvent(-1, true);
+        break;
+      case "KeyW":
+        this.dispatchMoveCartEvent(1, true);
+        break;
+      case "KeyS":
+        this.dispatchMoveCartEvent(-1, true);
+        break;
+      case "KeyE":
+        this.dispatchElevateCraneEvent(1, true);
+        break;
+      case "KeyD":
+        this.dispatchElevateCraneEvent(-1, true);
+        break;
+      default:
+        break;
     }
+  }
 
-    handleKeyUp(event) {
-        switch (event.code) {
-            case "KeyQ":
-                this.dispatchRotateCraneEvent(1, false);
-                break;
-            case "KeyA":
-                this.dispatchRotateCraneEvent(-1, false);
-                break;
-            case "KeyW":
-                this.dispatchMoveCartEvent(1, false);
-                break;
-            case "KeyS":
-                this.dispatchMoveCartEvent(-1, false);
-                break;
-            case "KeyE":
-                this.dispatchElevateCraneEvent(1, false);
-                break;
-            case "KeyD":
-                this.dispatchElevateCraneEvent(-1, false);
-                break;
-            default:
-                break;
-        }
+  handleKeyUp(event) {
+    switch (event.code) {
+      case "Digit1":
+        this.dispatchSwitchCameraEvent(0, false);
+        break;
+      case "Digit2":
+        this.dispatchSwitchCameraEvent(1, false);
+        break;
+      case "Digit3":
+        this.dispatchSwitchCameraEvent(2, false);
+        break;
+      case "Digit4":
+        this.dispatchSwitchCameraEvent(3, false);
+        break;
+      case "Digit5":
+        this.dispatchSwitchCameraEvent(4, false);
+        break;
+      case "Digit6":
+        this.dispatchSwitchCameraEvent(5, false);
+        break;
+      case "KeyQ":
+        this.dispatchRotateCraneEvent(1, false);
+        break;
+      case "KeyA":
+        this.dispatchRotateCraneEvent(-1, false);
+        break;
+      case "KeyW":
+        this.dispatchMoveCartEvent(1, false);
+        break;
+      case "KeyS":
+        this.dispatchMoveCartEvent(-1, false);
+        break;
+      case "KeyE":
+        this.dispatchElevateCraneEvent(1, false);
+        break;
+      case "KeyD":
+        this.dispatchElevateCraneEvent(-1, false);
+        break;
+      default:
+        break;
     }
+  }
 
-    dispatchSwitchCameraEvent(camera) {
-        const switchCameraEvent = new CustomEvent("switchCameraEvent", { detail: camera} );
-        document.dispatchEvent(switchCameraEvent);
-    }
+  dispatchSwitchCameraEvent(camera, isPressed) {
+    const switchCameraEvent = new CustomEvent("switchCameraEvent", {
+      detail: { camera: camera, isPressed: isPressed },
+    });
+    document.dispatchEvent(switchCameraEvent);
+  }
 
-    dispatchRotateCraneEvent(direction, isPressed) {
-        const rotateCraneEvent = new CustomEvent("rotateCraneEvent", { detail: { direction: direction, isPressed: isPressed } } );
-        document.dispatchEvent(rotateCraneEvent);
-    }
+  dispatchRotateCraneEvent(direction, isPressed) {
+    const rotateCraneEvent = new CustomEvent("rotateCraneEvent", {
+      detail: { direction: direction, isPressed: isPressed },
+    });
+    document.dispatchEvent(rotateCraneEvent);
+  }
 
-    dispatchMoveCartEvent(direction, isPressed) {
-        const moveCartEvent = new CustomEvent("moveCartEvent", { detail: { direction: direction, isPressed: isPressed } } );
-        document.dispatchEvent(moveCartEvent);
-    }
+  dispatchMoveCartEvent(direction, isPressed) {
+    const moveCartEvent = new CustomEvent("moveCartEvent", {
+      detail: { direction: direction, isPressed: isPressed },
+    });
+    document.dispatchEvent(moveCartEvent);
+  }
 
-    dispatchElevateCraneEvent(direction, isPressed) {
-        const elevateCranteEvent = new CustomEvent("elevateCranteEvent", { detail: { direction: direction, isPressed: isPressed } } );
-        document.dispatchEvent(elevateCranteEvent);
-    }
+  dispatchElevateCraneEvent(direction, isPressed) {
+    const elevateCranteEvent = new CustomEvent("elevateCranteEvent", {
+      detail: { direction: direction, isPressed: isPressed },
+    });
+    document.dispatchEvent(elevateCranteEvent);
+  }
 }
 
 class HUD {
+  constructor() {
+    this.initializeCameraKeys();
+    this.initializeRotateKeys();
+    this.initializeMoveKeys();
+    this.initializeElevateKeys();
+    document.addEventListener(
+      "switchCameraEvent",
+      this.handleSwitchCamera.bind(this)
+    );
+    document.addEventListener(
+      "rotateCraneEvent",
+      this.handleRotateCrane.bind(this)
+    );
+    document.addEventListener("moveCartEvent", this.handleMoveCart.bind(this));
+    document.addEventListener(
+      "elevateCraneEvent",
+      this.handleElevateCrane.bind(this)
+    );
+  }
+
+  // TODO:
+  initializeCameraKeys() {
+    let numberOfCameras = 6;
+    let currentPos = 0;
+    this.cameraSprites = [];
+  }
+
+  // TODO:
+  initializeRotateKeys() {}
+
+  // TODO:
+  initializeMoveKeys() {}
+
+  // TODO:
+  initializeElevateKeys() {}
+
+  // TODO:
+  handleSwitchCamera(event) {
+    if (event.detail.isPressed) {
+    } else {
+    }
+  }
+
+  // TODO:
+  handleRotateCrane(event) {
+    if (event.detail.isPressed) {
+    } else {
+    }
+  }
+
+  // TODO:
+  handleMoveCart(event) {
     // TODO
+    if (event.detail.isPressed) {
+    } else {
+    }
+  }
+
+  // TODO:
+  handleElevateCrane(event) {
+    // TODO
+    if (event.detail.isPressed) {
+    } else {
+    }
+  }
 }
 
-class CRANE {
-    // TODO
+class Crane {
+  // TODO
 }
 
-// TODO: this should be in a class
-let SCREEN_WIDTH = window.innerWidth;
-let SCREEN_HEIGHT = window.innerHeight;
-const aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-const frustumSize = 600;
+class Cameras {
+  constructor() {
+    let SCREEN_WIDTH = window.innerWidth;
+    let SCREEN_HEIGHT = window.innerHeight;
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('lightblue');
+    const aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+    const frustumSize = 600;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-document.body.appendChild(renderer.domElement);
+    const frontalCamera = new THREE.OrthographicCamera(
+      (-frustumSize * aspect) / 2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      1,
+      1000
+    );
+    frontalCamera.position.z = 100;
+    frontalCamera.lookAt(0, 0, 0);
 
-const frontalCamera = new THREE.OrthographicCamera(-frustumSize * aspect / 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
-frontalCamera.position.z = 100;
-frontalCamera.lookAt(0, 0, 0);
+    const lateralCamera = new THREE.OrthographicCamera(
+      (-frustumSize * aspect) / 2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      1,
+      1000
+    );
+    lateralCamera.position.x = 100;
+    lateralCamera.lookAt(0, 0, 0);
 
-const lateralCamera = new THREE.OrthographicCamera(-frustumSize * aspect / 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
-lateralCamera.position.x = 100;
-lateralCamera.lookAt(0, 0, 0);
+    const topCamera = new THREE.OrthographicCamera(
+      (-frustumSize * aspect) / 2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      1,
+      1000
+    );
+    topCamera.position.y = 100;
+    topCamera.lookAt(0, 0, 0);
 
-const topCamera = new THREE.OrthographicCamera(-frustumSize * aspect / 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
-topCamera.position.y = 100;
-topCamera.lookAt(0, 0, 0);
+    const fixedOrtCamera = new THREE.OrthographicCamera(
+      (-frustumSize * aspect) / 2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      1,
+      1000
+    );
+    fixedOrtCamera.position.set(new THREE.Vector3(100, 100, 100));
+    fixedOrtCamera.lookAt(0, 0, 0);
 
-const fixedOrtCamera = new THREE.OrthographicCamera(-frustumSize * aspect / 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
-fixedOrtCamera.position.set(new THREE.Vector3(100, 100, 100));
-fixedOrtCamera.lookAt(0, 0, 0);
+    const fixedPersCamera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+    fixedOrtCamera.position.set(new THREE.Vector3(100, 100, 100));
+    fixedPersCamera.lookAt(0, 0, 0);
 
-const fixedPersCamera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-fixedOrtCamera.position.set(new THREE.Vector3(100, 100, 100));
-fixedPersCamera.lookAt(0, 0, 0);
+    const movableCamera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 
-const movableCamera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+    this.cameras = [
+      frontalCamera,
+      lateralCamera,
+      topCamera,
+      fixedOrtCamera,
+      fixedPersCamera,
+      movableCamera,
+    ];
+    this.currentCamera = frontalCamera;
+    document.addEventListener(
+      "switchCameraEvent",
+      this.handleSwitchCamera.bind(this)
+    );
+  }
 
-const cameras = [frontalCamera, lateralCamera, topCamera, fixedOrtCamera, fixedPersCamera, movableCamera];
+  handleSwitchCamera(event) {
+    if (
+      !event.detail.isPressed ||
+      event.detail.camera === this.cameras.indexOf(this.currentCamera)
+    ) {
+      return;
+    }
 
-// TODO: remove cube
-const geometry = new THREE.BoxGeometry(100, 100, 100); // Adjusted size for better visibility
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    this.currentCamera = this.cameras[event.detail.camera];
+    console.log("Switched to camera: " + (event.detail.camera + 1));
+  }
+}
+
+class MainScene {
+  constructor(cameras) {
+    this.cameras = cameras;
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color("lightblue");
+
+    let SCREEN_WIDTH = window.innerWidth;
+    let SCREEN_HEIGHT = window.innerHeight;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    document.body.appendChild(this.renderer.domElement);
+
+    // TODO: remove cube
+    const geometry = new THREE.BoxGeometry(100, 100, 100);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+    this.scene.add(new THREE.AxesHelper(100000));
+    this.animate = this.animate.bind(this);
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.cameras.currentCamera);
+  }
+}
 
 new Input();
-let currentCamera = cameras[0];
-
-document.addEventListener("switchCameraEvent", switchCamera);
-
-function switchCamera(camera) {
-    console.log("Switched to camera " + (camera.detail + 1));
-    currentCamera = cameras[camera.detail];
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    renderer.render(scene, currentCamera);
-}
-
-animate();
-
+new HUD();
+let mainScene = new MainScene(new Cameras());
+mainScene.animate();
